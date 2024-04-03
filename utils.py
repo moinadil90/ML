854c70bb1e78269efd0821fc5e927f37d41a9681
moinadil90 <moinadil90@gmail.com>
2024-04-04 01:25:16 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+# Example utility functions (utils.py)
+def load_movie_data(data_path, filename):
+  # Load movie data from the specified file path
+  # ... (implementation to read movie data)
+
+def load_ratings_data(data_path, filename):
+  # Load ratings data from the specified file path
+  # ... (implementation to read ratings data)
